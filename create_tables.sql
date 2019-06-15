ed47ffa1e2594d1390f7c4c763b2ccb1ef7708ca
U-Matt-acer\Matt <matthewstringer@yahoo.com>
2019-06-15 11:01:15 -0400

    Adding MYSQL table schema, v0.0

---
+++ create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE activity_attribute(
+attribute_id INT AUTO_INCREMENT PRIMARY KEY,
+attribute_text VARCHAR(255)
+);
+
+CREATE TABLE activity_attribute_ref(
+activity_key INT NOT NULL,
+attribute_id INT NOT NULL REFERENCES activity_attribute(attribute_id),
+activity_has_attribute BOOLEAN NOT NULL
+);
